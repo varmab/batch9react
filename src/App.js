54b44f0f68e53abc71c2511d27db47d99ced7b9d
Varma Bhupatiraju <varmab@gmail.com>
2019-06-27 08:16:11 +0530

    Counter added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import Welcome from './Welcome'
+import Counter from './Counter'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Welcome title="Batch 9"/>
-        <Welcome title="Varma"/>
+        <Welcome year={2020} isGreeting={false}/>
+        <Welcome title="Varma" year={2019}/>
         </header>
+        <Counter/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Welcome extends Component{
     constructor(){
@@ -17,9 +18,26 @@ class Welcome extends Component{
 
     render(){
         return(
-            <h1 onClick={this.onTitleClick}>Welcome to {this.props.title} ({this.state.clicks})</h1>
+            <h1 onClick={this.onTitleClick}>Welcome to {this.props.title} ({this.state.clicks}) - {this.props.year}</h1>
         )
     }
+}
+
+Welcome.propTypes={
+    title(props, propName, component){
+        if(!(propName in props)){
+          return new Error(`missing ${propName}`)
+        }
+        if(props[propName].length < 3){
+          return new Error(`${propName} was too short`)
+        }
+      },
+    year:PropTypes.number.isRequired
+}
+
+Welcome.defaultProps={
+    title:"Batch 9",
+    year:2020
 }
 
 export default Welcome;
